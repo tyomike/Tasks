46751b29ce89d0652005ed185bc50dd0e0608f1d
Tyo Mike <misha96.tyo@yandex.ru>
2022-03-22 00:51:51 +0300

    'functions'

--- Functions/sumIntervals.php
+++ Functions/sumIntervals.php
@@ -1,12 +1,35 @@
 <?php
+//function sumIntervals ($array)
+//{
+//    $result = 0;
+//    for ($i = 0; $i < count($array); $i++) {
+//        for ($j = 0; $j < count($array[$i]); $j++) {
+//            $current = abs($array[1][$j] - $array[0][$j]);
+//            $result += $current;
+//        }
+//    }
+//    return $result;
+//}
 function sumIntervals ($array)
 {
     $result = 0;
-    for ($i = 0; $i < count($array); $i++) {
-        for ($j = 0; $j < count($array[$i]); $j++) {
-            $current = abs($array[1][$j] - $array[0][$j]);
-            $result += $current;
+    $firstItem = 0;//Первый параметр подмассива, из него будем вычитать второй параметр
+    $secondItem = 0;
+    foreach ($array as $currentKey => $currentValue)
+    {
+        foreach ($array as $nextKey => $nextValue)
+        {
+            if ($nextKey !== $currentKey)
+            {
+                if ($nextValue[0] < $currentValue[0] && $nextValue[0] > $currentValue[1] && $nextValue[1] )
+                {
+                    $firstItem += $nextValue[1];
+                }
+                if ($nextValue[0] > $currentValue[0] && $nextValue[0] < $currentValue[1])
+                {
+                    
+                }
+            }
         }
     }
-    return $result;
 }
